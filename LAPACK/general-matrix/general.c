c0faef93173fe2743370c2d6cd19abf94b3906fb
jpxx-arts <jpramalho123@hotmail.com>
2024-05-14 18:20:48 -0300

    .

--- LAPACK/general-matrix/general.c
+++ LAPACK/general-matrix/general.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int check_args(int argc, const char *file_name, const char *MATRIX_TYPE, const char *OPS, const int RANDOM_SEED){
+    if(argc != 6) {
+        printf("Use: %s <number_rows> <number_columns> <matrix_type> <ops> <seed>\n", file_name);
+        return -1;
+    }
+    if(strpbrk(MATRIX_TYPE, "gs") == NULL){
+        printf("Options to <matrix_type>: 'g', 's', 'gs'\n");
+        return -2;
+    }
+    if(strpbrk(OPS, "fis") == NULL){
+        printf("Options to <op>: 'f', 'i', 's'\n");
+        return -3;
+    }
+
+    return 0;
+}
 
 int create_matrix(float **matrix, int rows, int columns){
     (*matrix) = (float *) malloc(rows*columns * sizeof(int));
@@ -22,19 +40,20 @@ int auto_fill(float **matrix, int rows, int columns, int seed){
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            (*matrix)[j + (i*columns)] = rand();
+            (*matrix)[j + (i*columns)] = rand()%10;
         }
     }
 
     return 0;
 }
 
-int show_matrix(float *matrix, int rows, int columns){
+int show_matrix(float *matrix, int rows, int columns, char *matrix_name){
     if(matrix == NULL){
         printf("Empty matrix");
         return -1;
     }
 
+    printf("Matrix %s:\n", matrix_name);
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < columns; j++){
             printf("%.2f ", matrix[j + (i*columns)]);
--- LAPACK/general-matrix/main.c
+++ LAPACK/general-matrix/main.c
@@ -1,49 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mkl.h>
 #include "general.h"
 
-#define RANDOM_SEED 1
-
-int main(int argc, char const *argv[]){
+int main(int argc, const char *argv[]){
     double start_time, end_time;
     start_time = dsecnd();
 
-    if(argc != 3) {
-        printf("Use: %s <number_rows> <number_columns>\n", argv[0]);
-        return -1;
+    lapack_int n_rows = atoi(argv[1]), n_columns = atoi(argv[2]);
+    const char *MATRIX_TYPE = argv[3];
+    const char *OPS = argv[4];
+    const int RANDOM_SEED = atoi(argv[5]);
+
+    int err_code = check_args(argc, argv[0], MATRIX_TYPE, OPS, RANDOM_SEED);
+    if(err_code)
+        return err_code;
+
+    // Fluxo de tipo da matriz
+    if(!strcmp(MATRIX_TYPE, "g")){
+        // Criação de uma matriz A
+        float *A = NULL;
+        create_matrix(&A, n_rows, n_columns);
+
+        // Preenchimento de uma general matrix
+        auto_fill(&A, n_rows, n_columns, RANDOM_SEED);
+        
+        show_matrix(A, n_rows, n_columns, "A");
+    
+        // Fluxo de operações com a matriz
+        if(strpbrk(OPS, "f")){
+            //Fatoração de uma matriz A geral
+            lapack_int *ipiv = (lapack_int *) malloc(n_columns*sizeof(lapack_int));
+            lapack_int info_LU = LAPACKE_sgetrf(LAPACK_ROW_MAJOR, n_rows, n_columns, A, n_columns, ipiv);
+            
+            printf("LU info: %d\n", info_LU);
+            show_matrix(A, n_rows, n_columns, "A factorized");
+        }else if(strpbrk(OPS, "i")){
+
+        }else if(strpbrk(OPS, "s")){
+
+        }
+        
+    }else if(!strcmp(MATRIX_TYPE, "s")){
+        // Criação de uma matriz AP
+        float *AP = NULL;
+        int size = n_rows*(n_rows+1)/2;
+        create_matrix(&AP, 1, size);
+
+        // Preenchimento de uma packed matrix
+        packed_symmetric_fill(&AP, size, RANDOM_SEED);
+        
+        show_matrix(AP, n_rows, n_columns, "AP");
+    
+        //Fatoração de uma matriz A simétrica
     }
 
-    lapack_int n_rows = atoi(argv[1]), n_columns = atoi(argv[2]);
 
-    // //Fatoração de uma matriz A
-    float *A = (float *) malloc(n_rows*n_columns*sizeof(float));
-    symmetric_fill(&A, n_rows, n_columns, RANDOM_SEED);
-    // printf("Matrix A:\n");
-    // show_matrix(A, n_rows, n_columns);
-
-    lapack_int *ipiv = (lapack_int *) malloc(n_columns*sizeof(lapack_int));
-    lapack_int info_LU = LAPACKE_sgetrf(LAPACK_ROW_MAJOR, n_rows, n_columns, A, n_columns, ipiv);
-
-    // printf("LU info: %d\n", info_LU);
-    // printf("Matrix A factorized:\n");
-    // show_matrix(A, n_rows, n_columns);
+    // //Inversão de uma matriz A
+    // float *inv_A = (float *) malloc(n_rows*n_columns*sizeof(float));
+    // memcpy(inv_A, A, n_rows*n_columns);
+    // lapack_int info_inv = LAPACKE_sgetri(LAPACK_ROW_MAJOR, n_rows, inv_A, n_columns, ipiv);
+    // printf("inv info: %d\n", info_inv);
+    // printf("Inversed A:\n");
+    // show_matrix(inv_A, n_rows, n_columns);
 
     // //RHS
-    float *b = (float *) malloc(n_rows*sizeof(float));
-    auto_fill(&b, n_rows, 1, RANDOM_SEED);
+    // float *b = (float *) malloc(n_rows*sizeof(float));
+    // auto_fill(&b, n_rows, 1, RANDOM_SEED);
     // printf("Matrix b:\n");
     // show_matrix(b, n_rows, 1);
 
     // //Ax = b
-    lapack_int info_solved_system = LAPACKE_sgetrs(LAPACK_ROW_MAJOR, 'N', n_rows, 1, A, n_columns, ipiv, b , 1);
+    // lapack_int info_solved_system = LAPACKE_sgetrs(LAPACK_ROW_MAJOR, 'N', n_rows, 1, A, n_columns, ipiv, b , 1);
     // printf("solved system info: %d\n", info_solved_system);
-    printf("Matrix x:\n");
-    show_matrix(b, n_rows, 1);
+    // printf("Matrix x:\n");
+    // show_matrix(b, n_rows, 1);
 
-    free(b);
-    free(ipiv);
-    free(A);
+    // free(b);
+    // free(ipiv);
+    // free(inv_A);
+    // free(A);
 
     end_time = dsecnd();
     printf("%f s\n", end_time - start_time);
